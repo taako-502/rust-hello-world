5f0ca96b753182c42130232ae87a981ade73f284
taako-502 <yamamoto.takao502@gmail.com>
2025-05-29 21:31:12 +0900

    test: テストの追加とMikefileの追加

---
+++ Makefile
@@ -0,0 +1,8 @@
+.PHONY: run test
+default: run
+
+run:
+	cargo run
+
+test:
+	cargo test
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn get_message() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_message());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_message() {
+        assert_eq!(get_message(), "Hello, world!");
+    }
 }
